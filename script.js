5df0ffb01bc0d67f2459bf0ab7edf508b1ba65b0
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2023-03-10 09:23:05 +0300

    project done

--- script.js
+++ script.js
@@ -17,53 +17,47 @@ parallaxCards.forEach((element) => {
     textB.style.display = "none";
   }
 
-  function move(e) {
+  const move = (e) => {
     clearTimeout(timeSpend);
+    let x = e.offsetX;
+    let y = e.offsetY;
+    console.log([x, y]);
+    element.style.transition = "none";
 
-    let x = e.layerX;
-    let y = e.layerY;
-    console.log(x);
     if (element.classList.contains("parallax-card__rotate")) {
       imgB.style.transform = `translate(${((x - 200) / 5) * -1}px,${
         ((y - 175) / 5) * -1
       }px) scale(1.4)`;
       element.style.transform = `rotateY(${(x - 120) / 7}deg) rotateX(${
         180 + ((y - 205) / 7) * -1
-      }deg)`;
+      }deg) translateZ(-120px)`;
     } else {
       imgF.style.transform = `translate(${((x - 200) / 5) * -1}px,${
         ((y - 175) / 5) * -1
       }px)  scale(1.4)`;
       element.style.transform = `rotateY(${(x - 120) / 7}deg) rotateX(${
         ((y - 200) / 7) * -1
-      }deg)`;
+      }deg) translateZ(120px)`;
     }
-  }
+  };
 
-  element.addEventListener("mouseenter", () => {
-    element.addEventListener("mousemove", move);
-  });
+  element.addEventListener(
+    "mousemove",
+    (e) => {
+      move(e);
+    },
+    true
+  );
 
-  element.addEventListener("mouseleave", () => {
-    timeSpend = setTimeout(() => {
-      if (element.classList.contains("parallax-card__rotate")) {
-        element.style.transform = `rotateY(0deg) rotateX(180deg)`;
-        imgB.style.transform = `translate(0px,0px) scale(1.4)`;
-      } else {
-        element.style.transform = `rotateY(0deg) rotateX(0deg)`;
-        imgF.style.transform = `translate(0px,0px)  scale(1.4) rotateX(0deg)`;
-      }
-      element.removeEventListener("mousemove", move);
-    }, 2000);
-  });
-
-  element.addEventListener("click", () => {
+  element.onclick = () => {
     element.classList.toggle("parallax-card__rotate");
     front.classList.toggle("parallax-card__hide");
     back.classList.toggle("parallax-card__hide");
     if (element.classList.contains("parallax-card__rotate")) {
+      element.style.transition = "0.5s ease";
       element.style.transform = `rotateY(0deg) rotateX(180deg)`;
     } else {
+      element.style.transition = "0.5s ease";
       element.style.transform = `rotateY(0deg) rotateX(0deg)`;
     }
 
@@ -74,5 +68,18 @@ parallaxCards.forEach((element) => {
       textF.style.display = "block";
       textB.style.display = "none";
     }
-  });
+  };
+
+  element.onmouseleave = () => {
+    timeSpend = setTimeout(() => {
+      element.style.transition = "0.5s ease";
+      if (element.classList.contains("parallax-card__rotate")) {
+        element.style.transform = `rotateY(0deg) rotateX(180deg)`;
+        imgB.style.transform = `translate(0px,0px) scale(1.4)`;
+      } else {
+        element.style.transform = `rotateY(0deg) rotateX(0deg)`;
+        imgF.style.transform = `translate(0px,0px)  scale(1.4) rotateX(0deg)`;
+      }
+    }, 2000);
+  };
 });
